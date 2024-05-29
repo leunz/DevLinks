c5cf3a22d73d62d08e411034a1694a16ddb88ce0
Adir Leonardo <leonbrazille@gmail.com>
2024-05-29 14:40:42 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    // pegar a tag img
+    const img = document.querySelector('#profile img')
+
+    // substituir a imagem
+    if(html.classList.contains('light')){
+        // se tiver light mode, adicionar a imagem light
+        img.setAttribute("src", "./assets/avatar-light.png")
+        img.setAttribute("alt", "Foto camiseta preta com símbolo da Nike branco, celular com capa branca, fundo com 2 cores, metade pra esquerda branca e metade pra direita cinza escuro")
+    } else {
+        // se tiver sem light mode, manter a imagem normal
+        img.setAttribute("src", "./assets/avatar.png")
+    }
+
+
+
+
+}
